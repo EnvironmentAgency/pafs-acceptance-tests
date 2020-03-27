4eb8c5a5da5875b061ab20fac0a6b0c2cf307c39
Matt Hall <Matthew.Hall1@environment-agency.gov.uk>
2020-03-27 11:06:30 +0000

    [PM-587] standard of protection before and after

--- features/page_objects/standard_of_protection_coastal_after_page.rb
+++ features/page_objects/standard_of_protection_coastal_after_page.rb
@@ -2,10 +2,10 @@ class StandardOfProtectionCoastalAfterPage < SitePrism::Page
 
   section(:user_bar, AdminUserBarSection, AdminUserBarSection::SELECTOR)
 
-  element(:less_that_ten_years, "#standard_of_protection_coastal_after_step_coastal_protection_after_0")
-  element(:ten_to_nighteen_years, "#standard_of_protection_coastal_after_step_coastal_protection_after_1")
-  element(:twenty_to_fourtynine_years, "#standard_of_protection_coastal_after_step_coastal_protection_after_2")
-  element(:fifty_years_or_more, "#standard_of_protection_coastal_after_step_coastal_protection_after_3")
+  element(:less_that_ten_years, "#standard_of_protection_coastal_after_step_coastal_protection_after_0", visible: false)
+  element(:ten_to_nighteen_years, "#standard_of_protection_coastal_after_step_coastal_protection_after_1", visible: false)
+  element(:twenty_to_fourtynine_years, "#standard_of_protection_coastal_after_step_coastal_protection_after_2", visible: false)
+  element(:fifty_years_or_more, "#standard_of_protection_coastal_after_step_coastal_protection_after_3", visible: false)
 
   element(:submit_button, "input[name='commit']")
 
